661b7fcd280d48ba96ec5fc168469b1fc73ac771
ChrisSpaulding <ChrisSpaulding1991@gmail.com>
2017-10-18 20:26:02 -0600

    Renamed files correctly and separated out load and create files

--- HW3Create.sql
+++ HW3Create.sql
@@ -73,19 +73,6 @@ CREATE Table flight(
 );
 
 
-\Copy airport From 'C:\Users\chris_000\Documents\School\Database\FlightAndAirlineTbl\airport.csv' CSV HEADER;
-
-
-\Copy Carrier from 'C:\Users\chris_000\Documents\School\Database\FlightAndAirlineTbl\Carrier.csv' CSV HEADER;
-
-\Copy flight from  'C:\Users\chris_000\Documents\School\Database\FlightAndAirlineTbl\flights.csv' CSV HEADER;
-
-\Copy quarters From 'C:\Users\chris_000\Documents\School\Database\FlightAndAirlineTbl\Dates.csv' CSV HEADER;
-
-
-\Copy city From 'C:\Users\chris_000\Documents\School\Database\FlightAndAirlineTbl\City.csv' CSV HEADER;
-
-\Copy StateTable From 'C:\Users\chris_000\Documents\School\Database\FlightAndAirlineTbl\State.csv' CSV HEADER;
 
 ALTER TABLE airport
 ADD FOREIGN KEY (Abriviation) REFERENCES City(City_Abrv);
---
+++ HW3Load.sql
@@ -0,0 +1,14 @@
+
+\Copy airport From 'C:\Users\chris_000\Documents\School\Database\FlightAndAirlineTbl\airport.csv' CSV HEADER;
+
+
+\Copy Carrier from 'C:\Users\chris_000\Documents\School\Database\FlightAndAirlineTbl\Carrier.csv' CSV HEADER;
+
+\Copy flight from  'C:\Users\chris_000\Documents\School\Database\FlightAndAirlineTbl\flights.csv' CSV HEADER;
+
+\Copy quarters From 'C:\Users\chris_000\Documents\School\Database\FlightAndAirlineTbl\Dates.csv' CSV HEADER;
+
+
+\Copy city From 'C:\Users\chris_000\Documents\School\Database\FlightAndAirlineTbl\City.csv' CSV HEADER;
+
+\Copy StateTable From 'C:\Users\chris_000\Documents\School\Database\FlightAndAirlineTbl\State.csv' CSV HEADER;
