a64a5526cdeec94999760b972cac1f5b33e50ec6
ChrisSpaulding <ChrisSpaulding1991@gmail.com>
2017-10-18 19:35:32 -0600

    Home work 3 is now completed, sorry for how long it took.

--- ImportTablesIntoPostgress.sql
+++ ImportTablesIntoPostgress.sql
@@ -1,61 +1,113 @@
+
 CREATE TABLE StateTable (
-    State_Abrv varchar(2) PRIMARY KEY,
+    State_Abrv varchar(2), 
     FIPS int,
-    State_Name varchar(50)
+    State_Name varchar(50),
+	PRIMARY KEY (State_Abrv)
 	
 );
 
+--fk = state_abrv
 CREATE TABLE City (
     City_Name varchar(255),
-    City_Abrv varchar(5) Primary key, 
+    City_Abrv varchar(5) , 
     City_Market_ID int,
+	WAC int,
 	State_Abrv varchar(2),
-	State_Abrv varchar(2) FOREIGN KEY REFERENCES StateTable(State_Abrv) 
+	Primary key(City_Abrv)
 );
 
+-- fk ==abriviation (should be city_abvr)
 CREATE Table Airport (
-	Airport_ID varchar(15) PRIMARY KEY,
+	Airport_ID varchar(15),
 	Airport_SEQ_ID varchar(15),
-	City_Market_ID varchar(15) Foreign key references City_Abrv)
+	Abriviation varchar(15),
+	PRIMARY KEY (Airport_ID)
 	);
 	
+	
 CREATE Table Quarters (
-	MonthNumber int PRIMARY key,
-	Quarter int
+	MonthNumber int,
+	Quarter int,
+	PRIMARY key (MonthNumber)
 );
 
+
 CREATE Table Carrier(
-	Airline_Id varchar(15) PRIMARY key,
+	Airline_Id varchar(15),
 	CarrierAbv varchar(5),
 	Carrier_Entity varchar(10),
 	Carrier_Name varchar(255),
-	Unique_carrier varchar(5),
+	Unique_carrier varchar(15),
 	Unique_carrier_Entity varchar(15),
 	Unique_carrier_name varchar(255),
 	Carrier_group int,
 	Carrier_Group_New int,
 	Region varchar(255),
 	Start_Date_Source DATE,
-	Thru_Date_Source DATE	
+	Thru_Date_Source DATE,
+	PRIMARY key (Airline_Id)
 );
 
+-- fk= Airline_id; flight_month; Origin_Airport_Id; Dest_Airport_ID;  
 CREATE Table flight(
-	Airline_Id Foreign key references Carrier(Airline_Id),
+	Airline_Id varchar(15),
 	Flight_Year int, 
 	Flight_Month int, 
 	Ditance_Group int, 
 	Flight_Class varchar(5),
-	Dest_Airport_ID varchar(15) FOREIGN key references Airport(Airport_ID),
-	Origin_Airport_Id varchar(15) FOREIGN key references Airport(Airport_ID),
+	Dest_Airport_ID varchar(15),
+	Origin_Airport_Id varchar(15),
 	Departures_Scheduled int, 
 	Departures_Performed int, 
 	Payload int, 
 	Seats int, 
 	Passengers int, 
 	Freight int, 
+	Mail int,
 	Distance int, 
 	Ramp_to_Ramp int, 
 	Air_Time int, 
-	Distance_Group int
+	Distance_Group int,
+	PRIMARY KEY (Airline_Id, Dest_Airport_ID, Origin_Airport_Id, Flight_Year, Flight_Month, Flight_Class)
 );
 
+
+\Copy airport From 'C:\Users\chris_000\Documents\School\Database\FlightAndAirlineTbl\airport.csv' CSV HEADER;
+
+
+\Copy Carrier from 'C:\Users\chris_000\Documents\School\Database\FlightAndAirlineTbl\Carrier.csv' CSV HEADER;
+
+\Copy flight from  'C:\Users\chris_000\Documents\School\Database\FlightAndAirlineTbl\flights.csv' CSV HEADER;
+
+\Copy quarters From 'C:\Users\chris_000\Documents\School\Database\FlightAndAirlineTbl\Dates.csv' CSV HEADER;
+
+
+\Copy city From 'C:\Users\chris_000\Documents\School\Database\FlightAndAirlineTbl\City.csv' CSV HEADER;
+
+\Copy StateTable From 'C:\Users\chris_000\Documents\School\Database\FlightAndAirlineTbl\State.csv' CSV HEADER;
+
+ALTER TABLE airport
+ADD FOREIGN KEY (Abriviation) REFERENCES City(City_Abrv);
+
+ALTER TABLE city
+ADD FOREIGN KEY (State_Abrv) REFERENCES StateTable(State_Abrv);
+
+ALTER TABLE city
+ADD FOREIGN KEY (State_Abrv) REFERENCES StateTable(State_Abrv);
+
+
+ALTER TABLE flight
+ADD FOREIGN KEY (Airline_Id) REFERENCES Carrier(Airline_Id);
+
+
+ALTER TABLE flight
+ADD FOREIGN KEY (Flight_Month) REFERENCES quarters(monthnumber);
+
+ALTER TABLE flight
+ADD FOREIGN KEY (Origin_Airport_Id) REFERENCES Airport(Airport_ID);
+ 
+ 
+ALTER TABLE flight
+ADD FOREIGN KEY (Dest_Airport_ID) REFERENCES Airport(Airport_ID);
+ 
